9324cf4215e86d3b749c57123b36632151f0babf
Vijay Kumar <chittulurivijaykumar@gmail.com>
2023-08-26 13:59:24 +0530

    deployed

---
+++ js/script1.js
@@ -0,0 +1,23 @@
+//toggle icon  navbar
+
+//scroll section
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      //active navbar links
+      navLinks.forEach();
+    }
+  });
+  //sticky header
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+};
